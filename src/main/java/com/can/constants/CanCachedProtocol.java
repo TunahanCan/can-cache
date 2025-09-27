0ab17aa630f1dc08c4112ccf4465a9ca66ce4555
tnnhn <tunahan.can@hotmail.com.tr>
2025-09-27 15:20:59 +0300

    review code

--- src/main/java/com/can/constants/CanCachedProtocol.java
+++ src/main/java/com/can/constants/CanCachedProtocol.java
@@ -3,34 +3,34 @@ package com.can.constants;
 public interface CanCachedProtocol
 {
     // Protocol identification used for logging and diagnostics.
-    public static final String PROTOCOL = "cancached";
+    String PROTOCOL = "cancached";
 
     // Storage commands that either create or mutate values.
-    public static final String SET = "set";
-    public static final String ADD = "add";
-    public static final String APPEND = "append";
-    public static final String PREPEND = "prepend";
-    public static final String REPLACE = "replace";
-    public static final String CAS = "cas";
+    String SET = "set";
+    String ADD = "add";
+    String APPEND = "append";
+    String PREPEND = "prepend";
+    String REPLACE = "replace";
+    String CAS = "cas";
 
     // Retrieval commands.
-    public static final String GET = "get";
-    public static final String GETS = "gets";
+    String GET = "get";
+    String GETS = "gets";
 
     // Mutation commands that operate on numeric values.
-    public static final String INCR = "incr";
-    public static final String DECR = "decr";
+    String INCR = "incr";
+    String DECR = "decr";
 
     // Key management commands.
-    public static final String DELETE = "delete";
-    public static final String TOUCH = "touch";
+    String DELETE = "delete";
+    String TOUCH = "touch";
 
     // Server administration commands.
-    public static final String FLUSH_ALL = "flush_all";
-    public static final String STATS = "stats";
-    public static final String VERSION = "version";
-    public static final String QUIT = "quit";
+    String FLUSH_ALL = "flush_all";
+    String STATS = "stats";
+    String VERSION = "version";
+    String QUIT = "quit";
 
     // Generic error response used when a command cannot be processed.
-    public static final String ERROR = "ERROR";
+    String ERROR = "ERROR";
 }
--- src/main/java/com/can/constants/NodeProtocol.java
+++ src/main/java/com/can/constants/NodeProtocol.java
@@ -3,53 +3,53 @@ package com.can.constants;
 public interface NodeProtocol
 {
     // 'S' komutu, remote node'a gelen veriyi SET işlemi ile saklamak için gönderilir.
-    public static final byte CMD_SET = 'S';
+     byte CMD_SET = 'S';
 
     // 'X' komutu, Compare-And-Swap (CAS) operasyonu için kullanılır.
-    public static final byte CMD_CAS = 'X';
+     byte CMD_CAS = 'X';
 
     // 'G' komutu, istenen anahtardaki veriyi GET isteği olarak talep eder.
-    public static final byte CMD_GET = 'G';
+     byte CMD_GET = 'G';
 
     // 'D' komutu, belirtilen anahtarı DELETE isteği ile siler.
-    public static final byte CMD_DELETE = 'D';
+     byte CMD_DELETE = 'D';
 
     // 'C' komutu, node üzerindeki tüm veriyi CLEAR işlemi ile temizler.
-    public static final byte CMD_CLEAR = 'C';
+     byte CMD_CLEAR = 'C';
 
     // 'J' komutu, yeni bir node'un kümeye JOIN el sıkışmasını başlatmasını sağlar.
-    public static final byte CMD_JOIN = 'J';
+    byte CMD_JOIN = 'J';
 
     // 'R' komutu, bir node'dan tam veri akışı (STREAM) talep eder.
-    public static final byte CMD_STREAM = 'R';
+     byte CMD_STREAM = 'R';
 
     // 'H' komutu, uzak nodun anti-entropy Digest değerini istemek için gönderilir.
-    public static final byte CMD_DIGEST = 'H';
+    byte CMD_DIGEST = 'H';
 
     // 'O' yanıtı, isteğin başarılı olduğunu (OK) belirtir.
-    public static final byte RESP_OK = 'O';
+     byte RESP_OK = 'O';
 
     // 'H' yanıtı, GET sonucunda verinin bulunduğunu (HIT) bildirir.
-    public static final byte RESP_HIT = 'H';
+     byte RESP_HIT = 'H';
 
     // 'M' yanıtı, GET sonucunda verinin bulunamadığını (MISS) gösterir.
-    public static final byte RESP_MISS = 'M';
+     byte RESP_MISS = 'M';
 
     // 'T' yanıtı, boolean dönen işlemlerde TRUE sonucunu ifade eder.
-    public static final byte RESP_TRUE = 'T';
+    byte RESP_TRUE = 'T';
 
     // 'F' yanıtı, boolean dönen işlemlerde FALSE sonucunu ifade eder.
-    public static final byte RESP_FALSE = 'F';
+     byte RESP_FALSE = 'F';
 
     // 'A' yanıtı, join el sıkışmasının ACCEPT edildiğini bildirir.
-    public static final byte RESP_ACCEPT = 'A';
+    byte RESP_ACCEPT = 'A';
 
     // 'R' yanıtı, join isteğinin REDDEDİLDİĞİNİ/yeniden yönlendirilmesi gerektiğini belirtir.
-    public static final byte RESP_REJECT = 'R';
+     byte RESP_REJECT = 'R';
 
     // 1 baytlık chunk işaretleyicisi, STREAM yanıtında yeni bir kaydın geldiğini gösterir.
-    public static final byte STREAM_CHUNK_MARKER = 1;
+     byte STREAM_CHUNK_MARKER = 1;
 
     // 0 baytlık işaretleyici, STREAM yanıtında aktarımın sona erdiğini gösterir.
-    public static final byte STREAM_END_MARKER = 0;
+     byte STREAM_END_MARKER = 0;
 }
